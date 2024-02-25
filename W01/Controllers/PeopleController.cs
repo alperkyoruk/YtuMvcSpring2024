ad9497ea693ac93a96ec12a360e64f5b0458e2cc
Alper Yoruk <alperkyoruk@gmail.com>
2024-02-25 22:32:57 +0300

    ders 1

---
+++ W01/Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using W01.Models;
+
+namespace W01.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutMe()
+        {
+            People people1 = new People
+            {
+                Age = 21,
+                FullName = "John Doe",
+                Id = "122"
+            };
+            return View(people1);
+        }
+    }
+}
---
+++ W01/Models/People.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace W01.Models
+{
+    public class People
+    {
+        public string? Id { get; set; }
+        public int Age { get; set; }
+
+        public string? FullName { get; set; }
+    }
+}
